88bc4ec28831fcf89810b799532f4fc08bc44823
Ivan <ivan@Ivans-MacBook-Air.local>
2022-04-18 14:04:28 -0600

    small change

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -17,7 +17,7 @@ class HomePage extends Component {
 		}
     
     return (
-      <div className="homepage-tabs m-auto mt-4" style={style}>
+      <div className="homepage-tabs m-auto mt-4 mb-4" style={style}>
         <Tabs
           defaultActiveKey="unanswered"
           transition={false}
--- src/components/PostBoard.js
+++ src/components/PostBoard.js
@@ -7,16 +7,12 @@ class PostBoard extends Component {
 		const { questions, isAnswered } = this.props;
 		return (
 			<div>
-				{questions ? (
-					questions.map((question) =>
-						<Post key={question.id} id={question.id} isAnswered={isAnswered} />
-					)
-				) : (
-					<p> Empty Question List </p>
-				)}
+				{questions && (questions.map((question) =>
+					<Post key={question.id} id={question.id} isAnswered={isAnswered} />
+				))}
 				{!isAnswered && !questions.length &&
 					<h4 style={{ textAlign: 'center', color: 'gray' }}>
-						No More Questions! <Link to='/NewQuestion' style={{color:'green'}}>Make a new one!</Link>
+						No More Questions! <Link to='/NewQuestion' style={{ color: 'green' }}>Make a new one!</Link>
 					</h4>
 				}
 			</div>
